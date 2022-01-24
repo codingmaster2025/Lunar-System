0b79eb2e83d257227f8fd967ffc119741b58696f
codingmaster2025 <90358558+codingmaster2025@users.noreply.github.com>
2022-01-24 13:38:23 -0500

    Update CourseNameComparator.java

--- CourseNameComparator.java
+++ CourseNameComparator.java
@@ -1,9 +1,5 @@
 import java.util.Comparator;
 
-/*
- * 
- */
-
 public class CourseNameComparator implements Comparator {
         public int compare(Object o1, Object o2) {
                 Course e1 = (Course) o1;
